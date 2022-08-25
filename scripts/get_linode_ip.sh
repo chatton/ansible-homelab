23c1ec22e4df0517fe01bb980215ee69208774ea
Cian Hatton <cianhatton@gmail.com>
2022-08-25 21:26:20 +0100

    tempalte hosts file once the instance is created

--- scripts/get_linode_ip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-linode_ip="$(ansible-inventory -i linode.yml --list | jq ._meta.hostvars | jq '."simple-linode"' | jq -r .ipv4[0])"
-inventory_entry="root@${linode_ip}"
-echo $inventory_entry
-
-echo "[servers]" > linodehosts.ini
-echo "${inventory_entry}" >> linodehosts.ini
