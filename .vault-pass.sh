b1537377e87f89b4c1f5fb6a2ae64bd909feb4fe
Cian Hatton <cianhatton@gmail.com>
2022-09-02 20:21:15 +0100

    revert to using env var for pass, add bash_rc to qnap

--- .vault-pass.sh
+++ .vault-pass.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-# fetch vault password from bitwarden. We assume there is an item called "homelab-vault" that contains the password
-password="$(bw list items | jq -r 'map(select(.name == "homelab-vault"))[0].login.password')"
-echo "${password}"
+echo "${VAULT_PASS}"
